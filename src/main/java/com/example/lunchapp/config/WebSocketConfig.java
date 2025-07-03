7b1a942ac9b457f82a0c133772f7ae4a240fb32a
Ngo Gia Khanh <ngogiakhanh@MacBook-Pro-cua-Ngo.local>
2025-07-03 15:45:39 +0700

    fix function chat to admin

--- src/main/java/com/example/lunchapp/config/WebSocketConfig.java
+++ src/main/java/com/example/lunchapp/config/WebSocketConfig.java
@@ -2,13 +2,11 @@ package com.example.lunchapp.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.DefaultContentTypeResolver;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -23,25 +21,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Bean
-    public ThreadPoolTaskScheduler heartbeatScheduler() {
-        return new ThreadPoolTaskScheduler();
-    }
-
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/user", "/topic")
-                .setHeartbeatValue(new long[]{10000, 10000})
-                .setTaskScheduler(heartbeatScheduler());
+        registry.enableSimpleBroker("/user", "/topic");
         registry.setUserDestinationPrefix("/user");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
-                .withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*");
     }
 
     @Override
